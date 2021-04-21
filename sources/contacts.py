cbe556af34da9e9423e067275c63cb473c08cbd9
Olivier Cartier <me@cestoliv.com>
2021-04-21 16:02:26 +0200

    contacts.py cleaning

--- sources/contacts.py
+++ sources/contacts.py
@@ -5,70 +5,56 @@ import os
 import sys
 import inspect
 
+# Import local modules
 from db.db import DBConnection
 from users import get_profile, get_profile_username
 
 # Connection to database
 DB = DBConnection()
 
-"""AJOUTER UN CONTACT"""
-def add_contact(uid, contact_username) :
+####################
+# INTERNAL FUNCTIONS
+####################
 
-    user = get_profile_username(contact_username)
-    if user["status"] == "error" :
-        return {
-            "status": "error",
-            "code": "0001"
-        }
+def is_blocked(uid, contact_uid):
+    """
+    Checks if the given user has blocked the given contact
 
-    if user["uid"] == uid :
-        return {
-            "status": "error",
-            "code": "0003"
-        }
-
-    # test user still in contacts
-    user_contacts = get_contacts(uid)["contacts"]
-    for i in range(len(user_contacts)):
-        if user_contacts[i]["uid"] == user["uid"]:
-            return {
-                "status": "error",
-                "code": "0002"
-            }
-
-    date = datetime.datetime.now() #Date d'envoi du message : YYYY-MM-DD hh:mm:ss
-    t = [uid, date, user["uid"], 0]
+    Return a bool
+    Return false if users aren't in contact
+    """
 
     cur = DB.conn.cursor()
-    cur.execute("INSERT INTO contacts(uid, timestamp, contact_uid, blocked) values (?,?,?,?)", t)  #ajouter le contact dans la bdd
-    DB.conn.commit()
-
-    return {
-        "status": "success",
-        "uid": user["uid"],
-        "username": user["username"],
-        "name": user["name"]
-    }
-
-"""SAVOIR SI UN UTILISATEUR EST BLOQUE"""
-def is_blocked(uid, contact_uid) :
-    cur = DB.conn.cursor()
-    cur.execute('SELECT blocked FROM contacts WHERE uid = "' + uid + '" AND contact_uid = "' + contact_uid + '"')
+    cur.execute('SELECT blocked FROM contacts WHERE uid = ? AND contact_uid = ?',
+        (
+            uid,
+            contact_uid
+        )
+    )
     blocked=cur.fetchall()
+
     if len(blocked) == 0:
+        # aren't in contacts
         return False
     else:
         return blocked[0][0] == 1
 
-"""DERNIER MESSAGE ENTRE DEUX UTILISATEUR"""
 def last_message(sender_uid, receiver_uid):
+    """
+    Retrieves the last message sent between two users
+
+    Return a message object
+    """
 
     cur = DB.conn.cursor()
-    cur.execute('SELECT id, sender_uid, timestamp, content, seen FROM messages WHERE sender_uid = "'
-         + sender_uid + '" AND receiver_uid = "'
-         + receiver_uid + '" OR sender_uid = "'
-         + receiver_uid + '" AND receiver_uid = "'
-         + sender_uid + '" ORDER BY timestamp DESC LIMIT 1 OFFSET 0') #retrouver les messages dans la bdd
+    cur.execute('SELECT id, sender_uid, timestamp, content, seen FROM messages WHERE sender_uid = ? AND receiver_uid = ? OR sender_uid = ? AND receiver_uid = ? ORDER BY timestamp DESC LIMIT 1 OFFSET 0',
+        (
+            sender_uid,
+            receiver_uid,
+            receiver_uid,
+            sender_uid
+        )
+    )
 
     message = cur.fetchall()
     if len(message) > 0:
@@ -82,9 +68,14 @@ def last_message(sender_uid, receiver_uid):
     else:
         return {}
 
-"""SAVOIR SI 2 UTILISATEURS SONT EN CONTACTS"""
 def in_contacts(uid_user1, uid_user2):
+    """
+    Checks if the two given users are in contact (that they have added each other)
 
+    Return a boolean
+    """
+
+    # check that the first user is in contact with the second
     user1_with_user2 = False
     
     cur = DB.conn.cursor()
@@ -95,6 +86,7 @@ def in_contacts(uid_user1, uid_user2):
             if info_contacts_user1[i][0] == uid_user2:
                 user1_with_user2 = True
     
+    # check that the second user is in contact with the first
     user2_with_user1 = False
 
     cur = DB.conn.cursor()
@@ -107,14 +99,77 @@ def in_contacts(uid_user1, uid_user2):
     
     return (user1_with_user2 and user2_with_user1)
 
+###############
+# API FONCTIONS
+###############
 
-"""INFO SUR LES CONTACTS D'UN UTILISATEUR"""
+def add_contact(uid, contact_username) :
+    """
+    Add a contact to the given uid from the username
+
+    Error codes : 0001 => this contact does not exist
+                  0002 => you are already in contacts
+                  0003 => you can't add yourself as contact
+    """
+
+    contact_profile = get_profile_username(contact_username)
+    if contact_profile["status"] == "error":
+        # contact doesn't exist
+        return {
+            "status": "error",
+            "code": "0001"
+        }
+
+    if contact_profile["uid"] == uid:
+        # user and contact are the same person
+        return {
+            "status": "error",
+            "code": "0003"
+        }
+
+    # Check that users are not already in contact
+    user_contacts = get_contacts(uid)["contacts"]
+    for i in range(len(user_contacts)):
+        if user_contacts[i]["uid"] == contact_profile["uid"]:
+            # already in contacts
+            return {
+                "status": "error",
+                "code": "0002"
+            }
+
+    sending_date = datetime.datetime.now() # sending date : YYYY-MM-DD hh:mm:ss
+
+    cur = DB.conn.cursor()
+    cur.execute("INSERT INTO contacts(uid, timestamp, contact_uid, blocked) values (?,?,?,?)",
+        (
+            uid,
+            sending_date,
+            contact_profile["uid"],
+            0
+        )
+    )
+    DB.conn.commit()
+
+    return {
+        "status": "success",
+        "uid": contact_profile["uid"],
+        "username": contact_profile["username"],
+        "name": contact_profile["name"]
+    }
+
 def get_contacts(uid):
+    """
+    Returns the list of contacts of the given user
+
+    Error codes : no
+    """
+
     cur = DB.conn.cursor()
     cur.execute('SELECT contact_uid, timestamp FROM contacts WHERE uid =?', (uid,)) #prendre les uid de tous les contacts de l'utilisateur
 
     contacts_infos = cur.fetchall()
 
+    # get contacts detailed profile
     contacts_profile = [get_profile(contacts_infos[i][0]) for i in range(len(contacts_infos))]
 
     return {
@@ -133,62 +188,75 @@ def get_contacts(uid):
         ]
     }
 
-"""SUPPRIMER UN CONTACT"""
 def delete_contact(uid, contact_uid) :
+    """
+    Removes the given contact from the user's contacts
 
-    # test user in contacts
+    Error codes : 0001 => you are not in contact
+    """
+
+    # check that users are in contacts
     user_contacts = get_contacts(uid)["contacts"]
-    user_in_contact = False
     for i in range(len(user_contacts)):
         if user_contacts[i]["uid"] == contact_uid:
-            user_in_contact = True
-    if not user_in_contact:
-        return {
-            "status": "error",
-            "code": "0001"
-        }
-
-
+            # user are in contacts
+            return {
+                "status": "error",
+                "code": "0001"
+            }
+     
     cur = DB.conn.cursor()
-    cur.execute('DELETE FROM contacts WHERE uid = "' + uid + '" AND contact_uid = "' + contact_uid + '"')
-    cur.execute('DELETE FROM messages WHERE sender_uid = "' + uid + '" AND receiver_uid = "' + contact_uid + '"')
-    cur.execute('DELETE FROM messages WHERE sender_uid = "' + contact_uid + '" AND receiver_uid = "' + uid + '"')
+    # delete from user contacts
+    cur.execute('DELETE FROM contacts WHERE uid = ? AND contact_uid = ?', (
+        uid,
+        contact_uid
+    ))
+    # delete messages sended by user
+    cur.execute('DELETE FROM messages WHERE sender_uid = ? AND receiver_uid = ?', (
+        uid, 
+        contact_uid
+    ))
+    # delete messages sended by contact
+    cur.execute('DELETE FROM messages WHERE sender_uid = ? AND receiver_uid = ?', (
+        contact_uid,
+        uid
+    ))
     DB.conn.commit()
 
     return {
         "status": "success"
     }
 
-"""BLOQUER UN CONTACT"""
-def block_contact(uid, contact_uid) :
+def block_contact(uid, contact_uid):
+    """
+    Blocks the given contact
 
-    test_user_added_to_contacts = get_contacts(uid)
-    i = 0
-    user_in_contacts = False
-    while i<len(test_user_added_to_contacts["contacts"]) and user_in_contacts == False:
+    Error codes : 0001 => you are not in contact
+                  0002 => you have blocked this contact
+    """
 
-        if test_user_added_to_contacts["contacts"][i]["uid"] == contact_uid :
-            user_in_contacts = True
+    # check that users are in contacts
+    user_contacts = get_contacts(uid)["contacts"]
+    for i in range(len(user_contacts)):
+        if user_contacts[i]["uid"] == contact_uid:
+            # user are in contacts
+            return {
+                "status": "error",
+                "code": "0001"
+            }
 
-        else :
-            i+=1
-
-    if user_in_contacts == False :
-        return {
-            "status": "error",
-            "code": "0001"
-        }
-
-    test_user_blocked = is_blocked(uid, contact_uid)
-    if test_user_blocked == True :
+    # check that the contact is not blocked
+    if is_blocked(uid, contact_uid):
         return {
             "status": "error",
             "code": "0002"
         }
 
-
     cur = DB.conn.cursor()
-    cur.execute('UPDATE contacts SET blocked = 1 WHERE uid = "' + uid + '" AND contact_uid = "' + contact_uid + '"')
+    cur.execute('UPDATE contacts SET blocked = 1 WHERE uid = ? AND contact_uid = ?', (
+        uid,
+        contact_uid
+    ))
     DB.conn.commit()
 
     return {
@@ -196,36 +264,36 @@ def block_contact(uid, contact_uid) :
         "blocked": True
     }
 
-"""DEBLOQUER UN CONTACT"""
-def unblock_contact(uid, contact_uid) :
+def unblock_contact(uid, contact_uid):
+    """
+    Unblocks the given contact
 
-    test_user_added_to_contacts = get_contacts(uid)
-    i = 0
-    user_in_contacts = False
-    while i<len(test_user_added_to_contacts["contacts"]) and user_in_contacts == False:
+    Error codes : 0001 => you are not in contact
+                  0002 => you didn't block this contact
+    """
 
-        if test_user_added_to_contacts["contacts"][i]["uid"] == contact_uid :
-            user_in_contacts = True
+    # check that users are in contacts
+    user_contacts = get_contacts(uid)["contacts"]
+    for i in range(len(user_contacts)):
+        if user_contacts[i]["uid"] == contact_uid:
+            # user are in contacts
+            return {
+                "status": "error",
+                "code": "0001"
+            }
 
-        else :
-            i+=1
-
-    if user_in_contacts == False :
-        return {
-            "status": "error",
-            "code": "0001"
-        }
-
-    test_user_blocked = is_blocked(uid, contact_uid)
-    if test_user_blocked == False :
+    # check that the contact is not blocked
+    if not is_blocked(uid, contact_uid):
         return {
             "status": "error",
             "code": "0002"
         }
 
-
     cur = DB.conn.cursor()
-    cur.execute('UPDATE contacts SET blocked = 0 WHERE uid = "' + uid + '" AND contact_uid = "' + contact_uid + '"')
+    cur.execute('UPDATE contacts SET blocked = 0 WHERE uid = ? AND contact_uid = ?', (
+        uid,
+        contact_uid
+    ))
     DB.conn.commit()
 
     return {
